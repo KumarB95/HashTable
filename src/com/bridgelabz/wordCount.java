292c0081316535c0092cc4ec74102e28ace41aa6
KumarB95 <kumarbalingal@gmail.com>
2021-10-14 20:35:36 +0530

    Added : Finding frequency of words in sentence

---
+++ src/com/bridgelabz/wordCount.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class wordCount {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hash Table program");
+
+        //LinkedList<String, Integer> frequency = new LinkedList();
+        LinkedHashMap<String, Integer> frequency=new LinkedHashMap<>();
+        String message = "To be or not to be";
+
+        String[] messageArray = message.toLowerCase().split(" ");
+        for(String word : messageArray) {
+            String processed=word.toLowerCase();
+            if (frequency.containsKey(processed)) {
+                frequency.put(processed,frequency.get(processed)+1);
+            } else
+            {
+                frequency.put(processed,1);
+            }
+        }
+        //Display the linked list
+        System.out.println(frequency);
+    }
+}
